5edef5c01f5a9ca15a34ae760f0b6fd822ce8b10
Tareq Hassan <tareqhassan2014@gmail.com>
2021-08-22 23:09:34 +0600

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-function handelCustomization( id, extraCost) {
+let memoryCost = 0;
+let storageCost = 0;
+let deliveryCost = 0;
+
+function handelCustomization(id, extraCost) {
   document.getElementById(`${id}`).innerHTML = extraCost;
+
+  if (id == `memoryCost`) {
+    memoryCost = extraCost;
+  }
+  if (id == `storageCost`) {
+    storageCost = extraCost;
+  }
+  if (id == `deliveryCost`) {
+    deliveryCost = extraCost;
+  }
+
+  document.getElementById(`totalWithoutPromo`).innerHTML =
+    1299 + memoryCost + storageCost + deliveryCost;
 }
