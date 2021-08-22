bd3830acefddffaa0782ffd784d9cd265a611ca8
Tareq Hassan <tareqhassan2014@gmail.com>
2021-08-22 23:33:58 +0600

    first commit

--- app.js
+++ app.js
@@ -17,4 +17,18 @@ function handelCustomization(id, extraCost) {
 
   document.getElementById(`totalWithoutPromo`).innerHTML =
     1299 + memoryCost + storageCost + deliveryCost;
+  document.getElementById("totalwithpromo").innerHTML =
+    1299 + memoryCost + storageCost + deliveryCost;
+}
+
+function handelPromo() {
+  let promoInput = document.getElementById("promoInput").value;
+  if (promoInput == `amigoriv`) {
+    let totalWithoutPromo =
+      document.getElementById(`totalWithoutPromo`).innerHTML;
+    let discount = totalWithoutPromo - (totalWithoutPromo * 20) / 100;
+    document.getElementById("totalwithpromo").innerHTML = discount;
+  }else{
+    document.getElementById("worning").innerHTML = "tui sala borolok";
+  }
 }
